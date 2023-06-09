b4e982f108d3aa086958b88825484361e17679b6
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-06-09 11:27:09 -0400

    refactoring to use controllers

---
+++ server/constrollers/users.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
+
+const prisma = new PrismaClient()
+
+export const index = async (req: Request, res: Response) => {
+  const users = await prisma.users.findMany({
+    select: { id: true, first_name: true, last_name: true, email: true },
+  })
+  res.status(200).json(users)
+}
+
+export const show = async (req: Request, res: Response) => {
+  const users = await prisma.users.findUnique({
+    where: { id: parseInt(req.params.id) },
+    select: { id: true, first_name: true, last_name: true, email: true },
+  })
+  res.status(200).json(users)
+}
+
+export const store = async (req: Request, res: Response) => {
+  await prisma.users.create({
+    data: { ...req.body },
+  })
+  return res.sendStatus(201)
+}
+
+export const update = async (req: Request, res: Response) => {
+  await prisma.users.update({
+    where: { id: parseInt(req.params.id) },
+    data: { ...req.body },
+  })
+  return res.sendStatus(204)
+}
+
+export const destroy = async (req: Request, res: Response) => {
+  await prisma.users.delete({ where: { id: parseInt(req.params.id) } })
+  return res.sendStatus(204)
+}
