71a9885a546a2883e911e76176a0c66b1a3989ca
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-06-09 11:55:11 -0400

    user validation rules

--- server/routers/users.ts
+++ server/routers/users.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express'
+const { body } = require('express-validator')
+import validate from '../utils/validation'
 import { index, show, store, update, destroy } from '../constrollers/users'
 
 const router = Router()
@@ -7,9 +9,47 @@ router.route('/').get(index)
 
 router.route('/:id').get(show)
 
-router.route('/').post(store)
+router.route('/').post(
+  [
+    body('first_name')
+      .isString()
+      .isLength({ min: 3 })
+      .withMessage('The first_name must have minimum length of 3')
+      .trim(),
+    body('last_name')
+      .isString()
+      .isLength({ min: 3 })
+      .withMessage('The last_name must have minimum length of 3')
+      .trim(),
+    body('password').isString().withMessage('The password is required'),
+    //.isLength({ min: 8, max: 15 })
+    //.withMessage('The password must have min and max length between 8-15')
+    //.matches(/\d/)
+    //.withMessage('The password must have at least one number')
+    //.matches(/[!@#$%^&*(),.?":{}|<>]/)
+    //.withMessage('The password must have at least one special character'),
+  ],
+  validate,
+  store
+)
 
-router.route('/:id').put(update)
+router
+  .route('/:id')
+  .put(
+    [
+      body('first_name')
+        .isString()
+        .isLength({ min: 3 })
+        .withMessage('The first_name must have minimum length of 3')
+        .trim(),
+      body('last_name')
+        .isString()
+        .isLength({ min: 3 })
+        .withMessage('The last_name must have minimum length of 3')
+        .trim(),
+    ],
+    update
+  )
 
 router.route('/:id').delete(destroy)
 
