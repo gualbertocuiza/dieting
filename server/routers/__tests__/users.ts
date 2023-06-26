6242d0036017d4c5efc155f0f260f2406f3a29d0
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-06-26 11:17:48 -0400

    get users and find user by ID integration tests

--- server/routers/__tests__/users.ts
+++ server/routers/__tests__/users.ts
@@ -1,14 +1,28 @@
 import request from 'supertest'
-import app from '../index'
+import app from '../../app'
 
 describe('/users', () => {
   describe('GET /users', () => {
     it('should return json with a list of users', async () => {
       await request(app)
+        .get('/api/users')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+    })
+  })
+  describe('GET /users/:id', () => {
+    it('should return a user by ID', async () => {
+      await request(app)
         .get('/api/users/1')
         .set('Accept', 'application/json')
-        .expect('Content-Type', 'text/html; charset=utf-8')
-        .expect(404)
+        .expect(200)
+    })
+    it('should return 204 when no user found by ID', async () => {
+      await request(app)
+        .get('/api/users/123')
+        .set('Accept', 'application/json')
+        .expect(204)
     })
   })
 })
