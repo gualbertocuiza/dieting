1f10cc5cbaad4c030901c20e8c0fa41709c1cda4
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-06-26 16:58:37 -0400

    integration tests for users crud

--- server/routers/__tests__/users.ts
+++ server/routers/__tests__/users.ts
@@ -26,4 +26,55 @@ describe('/users', () => {
         .expect(204)
     })
   })
+  describe('POST /users', () => {
+    const user = {
+      first_name: 'Gcuiza',
+      last_name: 'Cuiza',
+      email: 'gcuiza@unos.com',
+      password: 'password',
+    }
+    it('should return 201 and create a new user', async () => {
+      await request(app)
+        .post('/api/users')
+        .set('Accept', 'application/json')
+        .send(user)
+        .expect(201)
+    })
+    it('should return 400 if first name is null', async () => {
+      await request(app)
+        .post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ ...user, first_name: '' })
+        .expect(400)
+    })
+    it('should return 400 if first name has less than three characters', async () => {
+      await request(app)
+        .post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ ...user, first_name: 'Gu' })
+        .expect(400)
+    })
+    it('should return 400 if last name is null', async () => {
+      await request(app)
+        .post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ ...user, last_name: '' })
+        .expect(400)
+    })
+    it('should return 400 if last name has less than three characters', async () => {
+      await request(app)
+        .post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ ...user, last_name: 'Cu' })
+        .expect(400)
+    })
+  })
+  describe('DELETE /users/:id', () => {
+    it('should return 204 and delete the found user', async () => {
+      await request(app)
+        .delete('/api/users/1')
+        .set('Accept', 'application/json')
+        .expect(204)
+    })
+  })
 })
