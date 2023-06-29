72759db2db886ff3744ae1795c8f94b012abcbf5
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-06-29 17:27:50 -0400

    cors setup

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,11 @@ import express, { json, urlencoded, Request, Response } from 'express'
 import { verifyToken } from './middlewares/authentication'
 import routers from './routers'
 import swagger from './routers/swagger'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => res.send('Hello World'))
