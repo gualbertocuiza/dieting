0625da56f9f23e982dfa6a57d3d8eba61ab4245a
gualberto cuiza <gualberto.cuiza@unosquare.com>
2023-07-06 10:04:54 -0400

    Refactoring register form validations

--- ui/src/components/auth/reg-form-validators.ts
+++ ui/src/components/auth/reg-form-validators.ts
@@ -10,24 +10,36 @@ import {
 export default {
   props: {
     first_name: { type: String },
-    password: {
-      type: String,
-    },
-    confirmation: {
-      type: String,
-    },
+    last_name: { type: String },
+    email: { type: String },
+    password: { type: String },
+    confirmation: { type: String },
   },
   setup(props, { slots }) {
-    const required = computed(() => [isRequired(props.first_name)])
-    const matching = computed(() => [
+    const firstNameRules = computed(() => [
+      isRequired(props.first_name),
+      isLength(props.first_name, 3),
+    ])
+    const lastNameRules = computed(() => [
+      isRequired(props.last_name),
+      isLength(props.last_name, 3),
+    ])
+    const emailRules = computed(() => [isValidEmail(props.email)])
+    const passwordRules = computed(() => [
+      isRequired(props.password),
+      isLength(props.password, 6),
+    ])
+    const passwordConfirmationRules = computed(() => [
       isMatching(props.password, props.confirmation),
     ])
-    const complexity = computed(() => calcComplexity(props.password))
+    //const complexity = computed(() => calcComplexity(props.password))
     return () =>
       slots.default({
-        required: required.value,
-        matching: matching.value,
-        complexity: complexity.value,
+        firstNameRules: firstNameRules.value,
+        lastNameRules: lastNameRules.value,
+        emailRules: emailRules.value,
+        passwordRules: passwordRules.value,
+        passwordConfirmationRules: passwordConfirmationRules.value,
       })
   },
 }
